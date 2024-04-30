825a463205ff41b8cb756cb1c4a2f03b171783fd
SS-Rana <146541878+SS-Rana@users.noreply.github.com>
2024-04-30 11:07:57 +0530

    commit

--- Day2_Q5.java
+++ Day2_Q5.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Day2_Q5 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        System.out.print(" Enter The Lenght in meter : ");
         double meter=sc.nextDouble();
 
         double feet=meter*3.37;

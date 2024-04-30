70a6a45844f8bdeb73541c31467b715f7cba959d
SS-Rana <146541878+SS-Rana@users.noreply.github.com>
2024-04-30 19:25:02 +0530

    Commit

---
+++ Day2_Q6.java
@@ -0,0 +1,15 @@
+public class Day2_Q6 {
+
+    public static void main(String[] args) {
+        System.out.println("Size of differnt variable types:");
+        System.out.println("Size of byte: "+Byte.SIZE+"bits");
+        System.out.println("Size of short: "+Short.SIZE+"bits");
+        System.out.println("Size of int: "+Integer.SIZE+"bits");
+        System.out.println("Size of long: "+Long.SIZE+"bits");
+        System.out.println("Size of float: "+Float.SIZE+"bits");
+        System.out.println("Size of double: "+Double.SIZE+"bits");
+        System.out.println("Size of char: "+Character.SIZE+"bits");
+        System.out.println("Size of boolean: Not defined preciesely In Java");
+
+    }
+}

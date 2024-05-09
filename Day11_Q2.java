5501ab80eebb8926bed0ce08cac958f2167509c9
SS-Rana <146541878+SS-Rana@users.noreply.github.com>
2024-05-09 11:41:13 +0530

    Commit

--- Day11_Q2.java
+++ Day11_Q2.java
@@ -1,13 +1,15 @@
 public class Day11_Q2 {
     public static void main(String[] args) {
         
-        int arr[]= {45,85,95,68,97,85,14,78,};
-        int max = arr[0];
-        for(int i=0; i<arr.length; i++){
-           
-
-            if( max<arr[i]){
-                max=arr[i];
+        int arr[]= {100,45,85,95,68,97,85,14,78,};
+        int max = Integer.MIN_VALUE;
+        int max2= Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max2 = max;
+                max = arr[i];
+            } else if (arr[i] > max2 && arr[i] != max) {
+                max2 = arr[i];
             }
         }
         System.err.print("\nThe element of the array are : ");
@@ -15,6 +17,6 @@ public class Day11_Q2 {
             System.err.print(arr[i]+" ");
         }
         System.err.println();
-        System.err.println("The Smallest element in the Given array  is "+max);
+        System.err.println("The 2nd largest element in the Given array  is "+max2);
     }
 }
---
+++ Day11_Q3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Day11_Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter the number of terms of fibonacci: ");
+        int num = sc.nextInt();
+        int first=0;
+        int second=1;
+        int arr[] = new int[num];
+
+        for(int i=0; i<num; i++){
+            arr[i]= first+second;
+            first=second;
+            second = arr[i];
+        }
+        System.err.print(num+" terms of fibonacci series : 0 1 ");
+        for(int i=0; i<num; i++){
+            System.err.print(arr[i]+" ");
+        }
+    }
+}
